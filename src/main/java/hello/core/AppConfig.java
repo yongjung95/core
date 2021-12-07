168ffdd0270708f02678eec3eb583d95213bd1b6
yongjung95@gmail.com <yongjung95@gmail.com>
2021-12-07 22:41:21 +0900

    # 21.12.07 김영한 : 스프링 핵심 원리 - 기본편
    스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 설정 정보 어노테이션
 public class AppConfig {
+    // IoC 제어의 역전 - 프로그램의 제어 흐름을 직접 제어하는 것이 아니라 외부에서 관리하는 것
+    // 프로그램에 대한 제어 흐름에 대한 권한은 모두 AppConfig 가 가지고 있기 때문이다.
 
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); // 생성자 의존관계 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy()); // 생성자 의존관계 주입
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
     }
 }
