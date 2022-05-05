6ae40199ecfa55f73ff6b7278926fa8dfccdf542
yongjung95@gmail.com <yongjung95@gmail.com>
2022-05-05 18:36:23 +0900

    # 22.05.05 김영한 : 스프링 핵심 원리 - 기본편
    빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,13 +28,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet(){
+
+    public void init (){
         connect();
         call("초기화 연결 메시지");
     }
-    @Override
-    public void destroy(){
+
+    public void close (){
         disconnect();
     }
 }
